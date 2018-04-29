5acebc5d3c721ec2dd74516ff62df1ade6aa3fd7
maqqju <matthewcroker@gmail.com>
2018-04-29 11:58:15 +0200

    Draft flow implemented. Deploying code to test on heroku due to CORS

--- upload/wistiaFileUpload.js
+++ upload/wistiaFileUpload.js
@@ -1,8 +1,37 @@
 angular.module("UploadToWistia").component("wistiaFileUpload", {
 	templateUrl : "upload/wistiaFileUpload.html",
-	controller : "WistiaFileUploadCtrl"
+	controller : "WistiaFileUploadCtrl",
+	bindings : {
+		apiPassword : "@"		
+	}
 }).controller("WistiaFileUploadCtrl", ["$scope","$element", function($scope, $element) {
 	$scope.options = {
-		url : "https://upload.wistia.com/"
+		url : "https://upload.wistia.com/",
+		singleFileUploads : true,
+		postMessage : true,
+		headers : {
+			"Content-Type" :"multipart/form-data"
+		}
 	}
+
+	$scope.$watch("$ctrl", function(o, n) {
+		if (!n.apiPassword) {
+			return;
+		}
+
+		$scope.options.formData = {
+			api_password : n.apiPassword
+		};
+
+	}, true);
+
+	var fileUploadScope = null;
+	var file = null;
+
+	$scope.$on("fileuploadadd", function(ev, data) {
+		fileUploadScope = data.scope;
+		file = data.files;
+
+		fileUploadScope.send(data);
+	});
 }]);
